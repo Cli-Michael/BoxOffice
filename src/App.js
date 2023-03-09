ff7d01a45329ac21241dd620c59c38206cc58c61
Cli-Michael <107493232+Cli-Michael@users.noreply.github.com>
2023-03-09 11:25:06 +0530

    Added React-Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+
+      <Route exact={true} path="/">
+        This is Home Page.
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred Page.
+      </Route>
+
+      <Route>
+        This is 404 Page.
+      </Route>
+
+    </Switch>
   );
 }
 
