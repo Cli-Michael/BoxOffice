c1ccd66e42388b26f67adc7c6c3c0e5ea4e6183f
Cli-Michael <107493232+Cli-Michael@users.noreply.github.com>
2023-03-22 00:59:43 +0530

    displayed show results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react';
-import MainPageLayout from '../components/MainPageLayout';
+import React, { useState } from "react";
+import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
 
   // Function to search the inputValue
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+        setResults(result);
+        // console.log(result);
       });
   };
 
-
-  const onInputChange = ev => {
+  const onInputChange = (ev) => {
     setInput(ev.target.value);
   };
 
-  const onKeyDown = ev => {
+  const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          { results.map((item) => ( <div key={item.show.id}>{item.show.name}</div> )) }
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -35,6 +51,8 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+
+      {renderResults()}
     </MainPageLayout>
   );
 };
