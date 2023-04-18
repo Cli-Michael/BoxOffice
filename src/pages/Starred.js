4be56841649dab6970491613bf5ea09c94e6a7d0
Cli-Michael <107493232+Cli-Michael@users.noreply.github.com>
2023-04-18 14:33:11 +0530

    Created Show Page Components and Populated them with data

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,8 +1,8 @@
-import React from "react";
-import MainPageLayout from "../components/MainPageLayout";
+import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
-  return <MainPageLayout>This is Starred</MainPageLayout>;
+  return <MainPageLayout>this is starred</MainPageLayout>;
 };
 
 export default Starred;
